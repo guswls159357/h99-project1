b14d3e3c2c68b97b34838e3f52145e6932d9cab1
guswls159357 <guswls159357@naver.com>
2022-01-24 01:41:31 +0900

    dto -> res, req dto

---
+++ src/main/java/com/basic/board/web/dto/BoardReqDto.java
@@ -0,0 +1,35 @@
+package com.basic.board.web.dto;
+
+import com.basic.board.domain.entity.Board;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class BoardReqDto {
+
+    @Size(max = 10, message = "제목은 10글자 이하여야 합니다")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해 주세요")
+    @Size(max = 50, message = "내용은 50글자 이하여야 합니다")
+    private String content;
+
+    @NotBlank(message = "이름을 입력해 주세요")
+    @Size(max = 6, message = "이름은 6글자 이하여야 합니다")
+    private String username;
+
+    public Board toEntity() {
+
+        return Board.builder()
+                .content(this.content)
+                .username(this.username)
+                .title(this.title)
+                .build();
+    }
+}
--- src/main/java/com/basic/board/web/dto/BoardResDto.java
+++ src/main/java/com/basic/board/web/dto/BoardResDto.java
@@ -5,23 +5,21 @@ import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class BoardDto {
+public class BoardResDto {
 
-    @Size(max = 10, message = "제목은 10글자 이하여야 합니다")
     private String title;
 
-    @NotBlank(message = "내용을 입력해 주세요")
-    @Size(max = 50, message = "내용은 50글자 이하여야 합니다")
     private String content;
 
-    @NotBlank(message = "이름을 입력해 주세요")
-    @Size(max = 6, message = "이름은 6글자 이하여야 합니다")
+    private LocalDateTime createdAt;
+
     private String username;
 
     public Board toEntity() {
